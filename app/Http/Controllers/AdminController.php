861e1e854f0729ac10ce3d2c628726e642463ba4
rawan-mortaja <rawanmortaja077@gmail.com>
2023-08-09 15:30:08 +0300

    basic laravel project

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
-     public function destroy(Request $request)
+    public function destroy(Request $request)
     {
         Auth::guard('web')->logout();
 
@@ -18,30 +18,32 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         $notification = array(
-            'message' => 'User Logout Successfully', 
+            'message' => 'User Logout Successfully',
             'alert-type' => 'success'
         );
 
         return redirect('/login')->with($notification);
-    } // End Method 
+    } // End Method
 
 
-    public function Profile(){
+    public function Profile()
+    {
         $id = Auth::user()->id;
         $adminData = User::find($id);
-        return view('admin.admin_profile_view',compact('adminData'));
+        return view('admin.admin_profile_view', compact('adminData'));
+    } // End Method
 
-    }// End Method 
 
-
-    public function EditProfile(){
+    public function EditProfile()
+    {
 
         $id = Auth::user()->id;
         $editData = User::find($id);
-        return view('admin.admin_profile_edit',compact('editData'));
-    }// End Method 
+        return view('admin.admin_profile_edit', compact('editData'));
+    } // End Method
 
-    public function StoreProfile(Request $request){
+    public function StoreProfile(Request $request)
+    {
         $id = Auth::user()->id;
         $data = User::find($id);
         $data->name = $request->name;
@@ -49,32 +51,32 @@ class AdminController extends Controller
         $data->username = $request->username;
 
         if ($request->file('profile_image')) {
-           $file = $request->file('profile_image');
+            $file = $request->file('profile_image');
 
-           $filename = date('YmdHi').$file->getClientOriginalName();
-           $file->move(public_path('upload/admin_images'),$filename);
-           $data['profile_image'] = $filename;
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['profile_image'] = $filename;
         }
         $data->save();
 
         $notification = array(
-            'message' => 'Admin Profile Updated Successfully', 
+            'message' => 'Admin Profile Updated Successfully',
             'alert-type' => 'info'
         );
 
         return redirect()->route('admin.profile')->with($notification);
+    } // End Method
 
-    }// End Method
 
-
-    public function ChangePassword(){
+    public function ChangePassword()
+    {
 
         return view('admin.admin_change_password');
+    } // End Method
 
-    }// End Method
 
-
-    public function UpdatePassword(Request $request){
+    public function UpdatePassword(Request $request)
+    {
 
         $validateData = $request->validate([
             'oldpassword' => 'required',
@@ -84,21 +86,19 @@ class AdminController extends Controller
         ]);
 
         $hashedPassword = Auth::user()->password;
-        if (Hash::check($request->oldpassword,$hashedPassword )) {
+        if (Hash::check($request->oldpassword, $hashedPassword)) {
             $users = User::find(Auth::id());
             $users->password = bcrypt($request->newpassword);
             $users->save();
 
-            session()->flash('message','Password Updated Successfully');
+            session()->flash('message', 'Password Updated Successfully');
             return redirect()->back();
-        } else{
-            session()->flash('message','Old password is not match');
+        } else {
+            session()->flash('message', 'Old password is not match');
             return redirect()->back();
         }
-
-    }// End Method
+    } // End Method
 
 
 
 }
- 
